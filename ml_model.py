b82b381c98aa3f172634a834bcee37daffdd4593
dwaraganathan <dkdwaraganathan@gmail.com>
2022-02-17 12:12:42 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,8 @@
 print("Adding all ML related code here")
+
+def model1():
+    print("adding more ml model1")
+def model2():
+    print("adding more ml model2")
+def model3():
+    print("adding more ml model3")
